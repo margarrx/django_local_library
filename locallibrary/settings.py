3db99032af4576e5a13baab3fe6a28b02e374617
Rafael Margary <margarrx@dukes.jmu.edu>
2025-05-13 11:12:33 -0400

    updates again

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -113,8 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Uncomment if you have extra static files and a directory in your GitHub repo.
 # If you don't have this directory and have this uncommented your build will fail
